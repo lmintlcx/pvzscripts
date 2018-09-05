de901c750086776f8f1e359b00c4a21146c8bb7e
lmintlcx <lmintlcx@gmail.com>
2018-09-05 20:10:40 +0800

    20180905

---
+++ tools/build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set SRC_DIR=C:\work\pvzscripts
+
+pythonw %SRC_DIR%\tools\md2html.py %SRC_DIR%\markdown\anjian.md %SRC_DIR%\html\anjian.html
+REM pythonw %SRC_DIR%\tools\md2html.py %SRC_DIR%\markdown\python.md %SRC_DIR%\html\python.html
+REM pythonw %SRC_DIR%\tools\md2html.py %SRC_DIR%\markdown\cpp.md %SRC_DIR%\html\cpp.html
---
+++ tools/md2html.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# usage: md2html.py src_file dst_file
+
+import os
+import sys
+import markdown
+
+
+def md2html(md_str):
+    html = """
+<html lang="zh-cn">
+<head>
+<meta charset="UTF-8">
+<title>PvZ Scripts</title>
+<link rel="icon" href="/favicon.ico">
+<link rel="stylesheet" href="normalize.css">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="styles/monokai.css">
+</head>
+<body>
+%s
+<script src="highlight.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
+"""
+
+    exts = ["markdown.extensions.extra"]
+
+    ret = markdown.markdown(md_str, extensions=exts)
+    return html % ret
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit()
+
+    src_file = open(sys.argv[1], "r", encoding="utf-8")
+    md = src_file.read()
+    src_file.close()
+
+    if os.path.exists(sys.argv[2]):
+        os.remove(sys.argv[2])
+
+    dst_file = open(sys.argv[2], "a", encoding="utf-8")
+    dst_file.write(md2html(md))
+    dst_file.close()
